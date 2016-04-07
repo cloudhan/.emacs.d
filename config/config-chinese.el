1bb37dbaa3babd983d992f94a081897defd5236c
Cloud Han <heliumhgy@gmail.com>
2016-04-07 20:34:48 +0800

    modified font setting

--- config/config-chinese.el
+++ config/config-chinese.el
@@ -4,12 +4,12 @@
 (when (eq system-type 'windows-nt)
   (set-default 'process-coding-system-alist
 	       '(("[pP][lL][iI][nN][kK]" gbk-dos . gbk-dos)
-		 ("[cC][mM][dD][pP][rR][oO][xX][yY]" gbk-dos . gbk-dos))))
+		 ("[cC][mM][dD][pP][rR][oO][xX][yY]" gbk-dos . gbk-dos)))
 
-(set-face-font 'default (font-spec :family "Consolas" :size 15))
-(dolist (charset '(han gb18030 chinese-gbk cjk-misc gb18030))
-  (set-fontset-font "fontset-default" charset
-		    (font-spec :family "微软雅黑")))
+  (set-face-font 'default (font-spec :family "Consolas" :size 15))
+  (dolist (charset '(han gb18030 chinese-gbk cjk-misc gb18030))
+    (set-fontset-font "fontset-default" charset
+		      (font-spec :family "微软雅黑"))))
 (setq face-font-rescale-alist
       '((".*微软雅黑.*" . 1.125)
 	(".*思源黑体.*" . 1.125)
